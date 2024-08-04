bdf5f1c9d931b02a1b865f3b0173f30dfda5da92
Platon Korchuganov <pdkorchuganov@gmail.com>
2024-08-04 12:36:33 +0300

    Language selection

--- hello/src/main/kotlin/Main.kt
+++ hello/src/main/kotlin/Main.kt
@@ -3,7 +3,28 @@ package com.jvmlab.platon
 
 fun main() {
     println("This is my first program")
-    print("What is your name? ")
-    val name = readln()
-    println("Hello $name!")
+
+    var lang = ""
+    while ((lang != "1") and (lang != "2")) {
+        println("Select your language")
+        println("Выберете ваш язык")
+        println("1 - English")
+        println("2 - русский")
+        print("Your choice / Ваш выбор:")
+        lang = readln()
+        println()
+        if ( ((lang != "1") and (lang != "2")) ) {
+            println("$lang: unknown language / неизвестный язык!")
+        }
+    }
+
+    if (lang == "1") {
+        print("What is your name? ")
+        val name = readln()
+        println("Hello $name!")
+    } else {
+        print("Как тебя зовут? ")
+        val name = readln()
+        println("Привет $name!")
+    }
 }
