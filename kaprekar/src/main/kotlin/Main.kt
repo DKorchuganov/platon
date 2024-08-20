1bbfa368a8a67e478cc713ee5affef2d1fdd3852
Denis Korchuganov <dkorchuganov@gmail.com>
2024-08-20 19:01:47 +0300

    big and small

--- kaprekar/src/main/kotlin/Main.kt
+++ kaprekar/src/main/kotlin/Main.kt
@@ -4,9 +4,16 @@ package com.jvmlab.platon.kaprekar
 fun main() {
 
     var input = ""
+
     while (!((input.length == 4)&&(input.all { it.isDigit() }))) {
         print("Enter 4-digit number: ")
         input = readln()
     }
+
+    val small = input.toList().sorted().joinToString("").toInt()
+    val big = input.toList().sortedDescending().joinToString("").toInt()
+    val result = big - small
+
+    println("$big - $small = $result")
 
 }
