2dcc750a59f343b24ddc85f22cdfe1d884ec75e5
Denis Korchuganov <dkorchuganov@gmail.com>
2024-08-22 19:06:31 +0300

    print cycle and check max digits

--- kaprekar/src/main/kotlin/Main.kt
+++ kaprekar/src/main/kotlin/Main.kt
@@ -6,11 +6,16 @@ fun main() {
     var input = ""
 
     while (!((input.isNotEmpty())&&(input.all { it.isDigit() }))) {
-        print("Enter number: ")
+        print("Enter number (max 18 digits): ")
         input = readln()
         if ((input.length > 1)&&input.all { it == input[0] }) {
             println()
             println("All characters should not be the same!")
+            input = ""
+        }
+        if (input.length > 18) {
+            println()
+            println("Too many characters: ${input.length}")
             input = ""
         }
     }
@@ -25,6 +30,15 @@ fun main() {
         val big = list.sortedDescending().joinToString("").toLong()
         result = big - small
         println("$big - $small = $result")
+    }
+
+    val resultIndex = oldResult.indexOf(result)
+    if (resultIndex == oldResult.lastIndex) {
+        println("Constant point: $result")
+    } else {
+        println("Cycle: ")
+        val cycle = oldResult.subList(resultIndex, oldResult.size).joinToString(" -> ")
+        println("$cycle -> $result")
     }
 
 }
