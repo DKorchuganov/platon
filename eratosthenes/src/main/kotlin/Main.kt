e04e9cd33219a9800d007fdc55319c6a0adf6892
Platon Korchuganov <pdkorchuganov@gmail.com>
2024-09-28 10:55:16 +0300

    -t flag

--- eratosthenes/src/main/kotlin/Main.kt
+++ eratosthenes/src/main/kotlin/Main.kt
@@ -4,62 +4,65 @@ import kotlin.math.sqrt
 
 
 fun main(args: Array<String>) {
-  val printPrimes: Boolean = ! booleanFlag('s', args)
+    val printPrimes = !booleanFlag('s', args)
+    val findTwins = booleanFlag('t', args)
 
-  var input = ""
+    var input = ""
 
-  while (!((input.isNotEmpty())&&(input.all { it.isDigit() }))) {
-    print("Enter number (max 9 digits): ")
-    input = readln()
+    while (!((input.isNotEmpty()) && (input.all { it.isDigit() }))) {
+        print("Enter number (max 9 digits): ")
+        input = readln()
 
-    if (input.length > 9) {
-      println()
-      println("Too many characters: ${input.length}")
-      input = ""
+        if (input.length > 9) {
+            println()
+            println("Too many characters: ${input.length}")
+            input = ""
+        }
     }
-  }
 
-  val size = input.toInt()
-  if (size < 3) {
+    val size = input.toInt()
+    if (size < 3) {
+        println()
+        println("The number should be greater than 2!!!")
+        return
+    }
+
+    val sieve = BooleanArray(size) {
+        true
+    }
+    sieve[0] = false
+    sieve[1] = false
+
+    var currentPrime: Int? = 2
+
+    val maxFind = sqrt(size.toDouble())
+    var twins = 0
+    var count = 0
+    while (currentPrime != null) {
+        count++
+        if (findTwins && sieve[currentPrime - 2]) {
+            println("${currentPrime - 2} and $currentPrime are twin primes")
+            twins++
+        }
+        val divisibles = if (currentPrime < maxFind) {
+            findDivisibles(currentPrime, sieve)
+        } else 0
+        if (printPrimes) {
+            println("$currentPrime: $divisibles")
+        }
+        currentPrime = nextPrime(currentPrime, sieve)
+    }
+    if (count == 1) {
+        println("1 prime number was found")
+    } else {
+        println("$count prime numbers were found")
+    }
+    if (findTwins) {
+        if (twins == 1) {
+            println("1 twin prime was found")
+        } else {
+            println("$twins twin primes were found")
+        }
+    }
     println()
-    println("The number should be greater than 2!!!")
-    return
-  }
-
-  val sieve = BooleanArray(size) {
-    true
-  }
-  sieve[0] = false
-  sieve[1] = false
-
-  var currentPrime: Int? = 2
-
-  val maxFind = sqrt(size.toDouble())
-  var twins = 0
-  var count = 0
-  while (currentPrime != null) {
-    count++
-    if (sieve[currentPrime - 2]) {
-      println("${currentPrime - 2} and $currentPrime are twin primes")
-      twins++
-    }
-    val divisibles = if (currentPrime < maxFind) {
-      findDivisibles(currentPrime, sieve)
-    } else 0
-    if (printPrimes) {
-      println("$currentPrime: $divisibles")
-    }
-    currentPrime = nextPrime(currentPrime, sieve)
-  }
-  if (count == 1) {
-    println("1 prime number was found")
-  } else {
-    println("$count prime numbers were found")
-  }
-  if (twins == 1) {
-    println("1 twin prime was found")
-  } else {
-    println("$twins twin primes were found")
-  }
-  println()
 }
