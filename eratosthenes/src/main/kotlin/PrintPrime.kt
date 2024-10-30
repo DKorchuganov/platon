42c1a1bc948ac06326000df053bebd95e0c28226
platon <pdkorchuganov@gmail.com>
2024-10-30 21:51:59 +0400

    make findDivisibles() private

--- eratosthenes/src/main/kotlin/PrintPrime.kt
+++ eratosthenes/src/main/kotlin/PrintPrime.kt
@@ -8,6 +8,18 @@ class PrintPrime(
 
     private var count = 0
 
+    private fun findDivisibles(currentPrime: Int, sieve: BooleanArray): Int {
+        var number = 0
+        for (i in (currentPrime * 2) .. sieve.lastIndex step currentPrime) {
+            if (sieve[i]) {
+                sieve[i] = false
+                number++
+            }
+        }
+
+        return number
+    }
+
     override fun process(currentPrime: Int) {
         val divisibles = if (currentPrime < maxFind) {
             findDivisibles(currentPrime, sieve)
