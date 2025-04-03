4ba18ea6043f0d3ac9fc0a7504ab65dd7ab582fb
Platon Korchuganov <pdkorchuganov@gmail.com>
2025-04-03 16:31:21 +0300

    ParallelSieve

--- wheel/src/main/kotlin/BasicSieve.kt
+++ wheel/src/main/kotlin/BasicSieve.kt
@@ -5,11 +5,11 @@ import kotlin.math.sqrt
 
 open class BasicSieve(private val size: Int) : Sieve {
     private val sieve = Array(8) { BooleanArray(size) { true } }
-    private val columnByRemainder = intArrayOf(
+    protected val columnByRemainder = intArrayOf(
         0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 2, 0, 3, 0, 0, 0, 4, 0, 5, 0, 0, 0, 6, 0, 0, 0, 0, 0, 7
     )
-    private val maxFind = sqrt(size.toDouble() * 30)
-    private val currentPrimePosition = Position(0, 1)
+    protected val maxFind = sqrt(size.toDouble() * 30)
+    protected val currentPrimePosition = Position(0, 1)
 
     final override var count = 4
         private set
@@ -21,7 +21,7 @@ open class BasicSieve(private val size: Int) : Sieve {
         sieve[0][0] = false
     }
 
-    private inner class Position(row: Int, column: Int) {
+    protected inner class Position(row: Int, column: Int) {
         private val remainderByColumn = longArrayOf(1, 7, 11, 13, 17, 19, 23, 29)
 
         var row = row
@@ -93,7 +93,7 @@ open class BasicSieve(private val size: Int) : Sieve {
     }
 
 
-    private fun removeCompositeColumn(productRow: Int, productColumn: Int) {
+    protected fun removeCompositeColumn(productRow: Int, productColumn: Int) {
         for (row in productRow .. sieve[productColumn].lastIndex step currentPrimePosition.value.toInt()) {
             sieve[productColumn][row] = false
         }
--- wheel/src/main/kotlin/ParallelSieve.kt
+++ wheel/src/main/kotlin/ParallelSieve.kt
@@ -1,5 +1,35 @@
 package com.jvmlab.platon.wheel
 
+import java.util.concurrent.Callable
+import java.util.concurrent.Executors
+
 class ParallelSieve(size: Int) : BasicSieve(size) {
 
+    private val threadPool = Executors.newFixedThreadPool(8)
+
+    override fun removeComposite() {
+
+        if (currentPrimePosition.value > maxFind) {
+            threadPool.shutdown()
+            return
+        }
+
+        val nextNumberPosition = Position(currentPrimePosition)
+
+        val tasks = mutableListOf<Callable<Unit>>()
+
+        repeat(8) {
+            val product = currentPrimePosition.value * nextNumberPosition.value
+            val productRow = (product / 30).toInt()
+            val productColumn = columnByRemainder[(product % 30).toInt()]
+
+            tasks.add {
+                removeCompositeColumn(productRow, productColumn)
+            }
+
+            nextNumberPosition.next()
+        }
+
+        threadPool.invokeAll(tasks)
+    }
 }
