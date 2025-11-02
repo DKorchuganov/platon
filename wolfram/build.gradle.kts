24ce4d65e353c2859a1a52df139016171b4bc1f9
platon <pdkorchuganov@gmail.com>
2025-11-02 18:14:49 +0400

    Wolfram initial commit

---
+++ wolfram/build.gradle.kts
@@ -0,0 +1,44 @@
+plugins {
+    kotlin("jvm") version "2.1.0"
+    application
+}
+
+group = "com.jvmlab.platon"
+version = "1.0-SNAPSHOT"
+
+application {
+    mainClass.set("com.jvmlab.platon.wolfram.MainKt")
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("com.jvmlab.platon:cli")
+    testImplementation(kotlin("test"))
+}
+
+tasks {
+    val fatJar = register<Jar>("fatJar") {
+        dependsOn.addAll(listOf("compileJava", "compileKotlin", "processResources")) // We need this for Gradle optimization to work
+        archiveClassifier.set("standalone") // Naming the jar
+        duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+        manifest { attributes(mapOf("Main-Class" to application.mainClass)) } // Provided we set it up in the application plugin configuration
+        val sourcesMain = sourceSets.main.get()
+        val contents = configurations.runtimeClasspath.get()
+            .map { if (it.isDirectory) it else zipTree(it) } +
+                sourcesMain.output
+        from(contents)
+    }
+    build {
+        dependsOn(fatJar) // Trigger fat jar creation during build
+    }
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+kotlin {
+    jvmToolchain(21)
+}
---
+++ wolfram/src/main/kotlin/Main.kt
@@ -0,0 +1,8 @@
+package com.jvmlab.platon.wolfram
+
+import com.jvmlab.platon.cli.BooleanOption
+import com.jvmlab.platon.cli.Parser
+
+
+fun main(args: Array<String>) {
+}
