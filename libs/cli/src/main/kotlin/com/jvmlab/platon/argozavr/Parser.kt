a6ccdd64a94384e6ce85fcd4ec93ab19c1213564
Platon Korchuganov <pdkorchuganov@yandex.ru>
2026-01-04 12:52:16 +0400

    Parser string option

--- libs/cli/src/main/kotlin/com/jvmlab/platon/argozavr/Parser.kt
+++ libs/cli/src/main/kotlin/com/jvmlab/platon/argozavr/Parser.kt
@@ -1,5 +1,21 @@
 package com.jvmlab.platon.argozavr
 
 class Parser {
+    private val optionsByShortName = mutableMapOf<Char, AnyOption<*>>()
+    private val optionsByLongName = mutableMapOf<String, AnyOption<*>>()
 
+
+    fun stringOption(shortName: Char, longName: String, description: String): AnyOption<*> {
+        val option = StringOption(shortName, longName, description)
+        optionsByShortName[shortName] = option
+        optionsByLongName[longName] = option
+        return option
+    }
+
+
+    fun stringOption(longName: String, description: String): AnyOption<*> {
+        val option = StringOption(null, longName, description)
+        optionsByLongName[longName] = option
+        return option
+    }
 }
